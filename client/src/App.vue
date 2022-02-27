776d2f98b67279918824d153919e5e0ee89ad8a6
Shivangi Kakkar <skakkar@skakkar-l5jgh5.lan>
2022-02-27 17:21:59 -0500

    adding client folder and components

---
+++ client/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Nav from './components/Nav.vue';
+import Messages from './pages/Messages.vue';
+</script>
+
+<template>
+  <Nav />
+  <Messages />
+  <h1 class = "title">Praying Ukriane</h1>
+</template>
+
+<style>
+
+</style>
