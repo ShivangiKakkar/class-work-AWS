8e684cdd8dbf376ea7d3aff137db9d0f362b39e5
ShivangiKakkar <35342019+ShivangiKakkar@users.noreply.github.com>
2022-03-21 19:21:44 +0000

    Deploying to node-deploy from @ ShivangiKakkar/class-work-AWS@d51df6849913d8502f751f6b139bbffaf99093d1 🚀

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express') //common JS
+
+const usersController = require('./controllers/users');
+
+//pipeline message comes in -> begining of express-> goes through some stuff then gives bac to client
+const app = express() 
+const port = 3000
+
+app
+    .use('/', express.static(__dirname + '/public/'))
+
+    .get('/', (req, res) => {
+       es.send('Heyy, you are on the homepage');
+    })
+    .use('/users', usersController) //mouting that controller at thus path
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at port http://localhost:${port}`)
+})
