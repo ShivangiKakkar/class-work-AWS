ab633a073ce535916713f84848447dc6d49e21bb
Shivangi Kakkar <skakkar@client157-139.wireless.newpaltz.edu>
2022-03-28 15:18:42 -0400

    moving forward with server

--- server/models/user.js
+++ server/models/user.js
@@ -30,18 +30,39 @@ const list = [
     id: 3,
 },
 ];
-
 function get(id){
-    return list.find(user => user.id === parseInt(id));
+    return {...list.find(user => user.id === parseInt(id)), password: undefined};
 }
+
+function remove(id){
+    const index = list.findIndex(user => user.id === parseInt(id));
+    const user = list.splice(index,1);
+    
+    return {...user[0], password: undefined} ;
+}
+
+function update(id, newUser){
+    const index = list.findIndex(user => user.id === parseInt(id));
+    const oldUser = list[index];
+    newUser = list[index] = {...oldUser, ...newUser };
+    return {...newUser, password: undefined} ;
+}
+
+
 
 module.exports = {
     create(user){
         user.id = ++highestId;
         list.push(user);
-        return user;
+        return {...user, password: undefined};
+    },
+    remove,
+    update,
+    get list(){
+        return list.map(x=> ({...x, password: undefined}) );
     }
 }
+//getter -> every part of program 
 
-module.exports.list = list;
+// one option -> module.exports.list = ()=>list.map(x=>({...x, password: undefined}) );
 module.exports.get = get;
