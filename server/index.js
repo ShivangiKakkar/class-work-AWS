28c3f8b62433dc882af024100120053b1c481d3a
Shivangi Kakkar <skakkar@client158-182.wireless.newpaltz.edu>
2022-03-31 14:28:34 -0400

    setting .env

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+
+require('dotenv').config();
 const express = require('express') //common JS
 
 const usersController = require('./controllers/users');
@@ -5,6 +7,7 @@ const usersController = require('./controllers/users');
 //pipeline message comes in -> begining of express-> goes through some stuff then gives bac to client
 const app = express() 
 const port = process.env.PORT || 3000;
+//console.log(process.env);
 
 app
     .use('/', express.static(__dirname + '/public/'))
@@ -12,7 +15,7 @@ app
     //.use(express.urlencoded({ extended: true }))
 
     .get('/api/', (req, res) => {
-       res.send('Heyy, you are on the homepage');
+       res.send('You are at the root of the API. For the best class ever - ' + process.env.BEST_CLASS_EVER);
     })
     .use('/api/users', usersController) //mouting that controller at thus path
 
