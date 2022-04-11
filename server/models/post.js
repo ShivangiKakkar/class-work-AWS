236325fe8b66f67010ec3784e3cb9860e4256328
ShivangiKakkar <35342019+ShivangiKakkar@users.noreply.github.com>
2022-04-11 19:54:26 +0000

    Deploying to node-deploy from @ ShivangiKakkar/class-work-AWS@83ee55caddba58dd7e12ca78e9b64538f8e16ad9 🚀

--- server/models/post.js
+++ server/models/post.js
@@ -1,13 +1,16 @@
 
-
+const { db, ObjectId } = require('./mongo');
 const userModel = require('./user');
+
+const collection = db.db("journal").collection("posts");
+
 let highestId = 3;
 
 const list = [
 {
     src: 'https://media-cldnry.s-nbcnews.com/image/upload/t_focal-1120x560,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg',
     caption: 'Russian forces focus on eastern Ukraine amid rising fears they may seek to split the country in two',
-    owner: 1,
+    owner: 'shivangikakkar',
     likes: [],
     comments: [],
     isPublic: true,
@@ -16,7 +19,7 @@ const list = [
 {
     src: 'https://media-cldnry.s-nbcnews.com/image/upload/t_focal-1120x560,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg',
     caption: 'Russian forces focus on eastern Ukraine amid rising fears they may seek to split the country in two',
-    owner: 1,
+    owner: 'shivangikakkar',
     likes: [],
     comments: [],
     isPublic: true,
@@ -25,7 +28,7 @@ const list = [
 {
     src: 'https://media-cldnry.s-nbcnews.com/image/upload/t_focal-1120x560,f_auto,q_auto:best/newscms/2022_13/3543963/220328-ukriane-mb-0729.jpg',
     caption: 'Russian forces focus on eastern Ukraine amid rising fears they may seek to split the country in two',
-    owner: 2,
+    owner: 'russian_dictator',
     likes: [],
     comments: [],
     isPublic: true,
@@ -34,40 +37,47 @@ const list = [
 
 ];
 
-const includeUser = post => ({ ...post, user: userModel.get(post.owner) });
+const includeUser = async post => ({ ...post, user: await userModel.getByHandle(post.owner) });
 
-function get(id){
-    const post = list.find(x => x.id === parseInt(id));
+async function get(id){
+    const post = await collection.findOne({_id: new ObjectId(id)});
     if(!post){
         throw { status: 404, message: 'Post not found' };
     }
     return includeUser(post);
 }
 
-function remove(id){
-    const index = list.findIndex(x => x.id === parseInt(id));
-    const post = list.splice(index,1);
+async function remove(id){
+    const post = await collection.findOneAndDelete({ _id: new ObjectId(id)});
     
-    return includeUser(post[0]);
+    return includeUser(post.value);
 }
 
-function update(id, newPost){
-    const index = list.findIndex(x => x.id === parseInt(id));
-    const oldPost = list[index];
-    newPost = list[index] = {...oldPost, ...newPost };
+async function update(id, newPost){
+    newPost = await collection.findOneAndUpdate(
+        { _id: new ObjectId(id)}, 
+        { $set: newPost }, 
+        { returnDocument: 'after' });
     return includeUser(newPost);
 }
 
 module.exports = {
-    create(post){
+    async create(post){
         post.id = ++highestId;
-        list.push(post);
+
+        const result = await collection.insertOne(post);
+        post = await get(result.insertedId);
+
         return includeUser(post);
     },
     remove,
     update,
-    get list(){
-        return list.map(x => includeUser(x));
+    async getList(){
+        const posts = await collection.find({}).toArray();
+
+        return Promise.all( posts.map(x => includeUser(x)));
+        
+        //promise all means you want all of these promises to happen
     }
 }
 //getter -> every part of program 
