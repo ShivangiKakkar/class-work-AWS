139e02fbeb924decf552ca072afe40d07eceb344
ShivangiKakkar <35342019+ShivangiKakkar@users.noreply.github.com>
2022-04-21 18:54:24 +0000

    Deploying to node-deploy from @ ShivangiKakkar/class-work-AWS@5f366c914ce7086e916e32bcc997ce2cdf9a78c1 🚀

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app
 
     .use(function(req, res, next){
       res.header("Access-Control-Allow-Origin", "*"); //list server instead of * for firefox
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
       next();
     })
     .use(express.json())
