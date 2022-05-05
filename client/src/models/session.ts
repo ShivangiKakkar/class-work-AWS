c5d8813f3a6c8e4ea97a4fc8aba5446f2528d66f
Shivangi Kakkar <skakkar@client156-27.wireless.newpaltz.edu>
2022-05-05 14:19:57 -0400

    created react app

--- client/src/models/session.ts
+++ client/src/models/session.ts
@@ -17,11 +17,20 @@ export const useSession = defineStore('session', {
 
         async GoogleLogin(){
             await loadScript('https://accounts.google.com/gsi/client','google-signin');
-            google.accounts.id.initialize({
+            const auth_client = google.accounts.oauth2.initTokenClient({
                 client_id: <string>import.meta.env.VITE_GOOGLE_CLIENT_ID,
-                callback: x => {
-                    const user = decodeJWT(x.credential);
+                scope: 'email profile',
+                //scope is usually url, its telling 
+                callback: async x => {
+                    //https://www.googleapis.com/calendar/v3/
+                    const user = await fetch('https://www.googleapis.com/oauth2/v3/userinfo?alt=json', {
+                        headers: {
+                            Authorization: `Bearer ${x.access_token}`,
+                        },
+                    }).then(x => x.json());
                     console.log(user);
+                    // const user = decodeJWT(x.credential);
+                    // console.log(user);
                     this.user = {
                         id: user.sub,
                         email: user.email,
@@ -33,7 +42,8 @@ export const useSession = defineStore('session', {
                     }
                 }
               });
-              google.accounts.id.prompt(()=>{});
+              auth_client.requestAccessToken();
+            //   google.accounts.id.prompt(()=>{});
 
         },
         async Login(email: string, password: string) {
